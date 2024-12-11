d2e26575662707b8386437e923ee9b1f20cdbbba
Saurav <sauravsynapse@gmail.com>
2024-12-11 11:30:34 +0530

    Course 17 updates

--- src/main/java/com/jobex/jobms/job/clients/ReviewClient.java
+++ src/main/java/com/jobex/jobms/job/clients/ReviewClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name="REVIEWMS")
+@FeignClient(name="REVIEWMS", url="${review-service.url}")
 public interface ReviewClient {
 
     @GetMapping("/reviews")
